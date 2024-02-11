bed58fd5a6b482e5d5f5e7ae6b08e36be1cdcafd
Marwen <marwen.jlassi.2020@gmail.com>
2024-02-11 23:55:27 +0100

    0.0.1

--- test/app.js
+++ test/app.js
@@ -40,31 +40,24 @@ const imagee = document.getElementById("ima");
 let currentIndex = 0;
 
 function changeImage() {
-    // Apply scaling and opacity transitions
     imagee.style.transition = "transform 0.5s ease, opacity 0.5s ease";
 
-    // Scale down and fade out the current image
     imagee.style.opacity = "0";
     imagee.style.transform = "scale(0.8)";
 
     setTimeout(() => {
-        // Change the image source
         imagee.src = imageUrls[currentIndex];
 
-        // Scale up and fade in the new image
         setTimeout(() => {
             imagee.style.opacity = "1";
             imagee.style.transform = "scale(1)";
-        }, 100); // Wait for a brief moment before fading in and scaling up
-    }, 500); // Wait for the fade out transition to complete
+        }, 100);
+    }, 500);
 
-    // Update currentIndex for the next image
     currentIndex = (currentIndex + 1) % imageUrls.length;
 }
 
-// Initial image display
 changeImage();
 
-// Automatic filling and changing every 3 seconds
 setInterval(changeImage, 3000);
 
